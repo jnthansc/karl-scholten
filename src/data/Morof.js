ec76f847fe09011321fee605dbbfa364f099e955
Jonathan <jona.schmidt99@web.de>
2022-04-12 16:16:26 +0200

    adjusted icon and added missing pictures

--- src/data/Morof.js
+++ src/data/Morof.js
@@ -3,11 +3,15 @@ import morof2 from '../assets/Serien_Karl/Morof/2.jpg';
 import morof3 from '../assets/Serien_Karl/Morof/3.jpg';
 import morof4 from '../assets/Serien_Karl/Morof/4.jpg';
 import morof5 from '../assets/Serien_Karl/Morof/5.jpg';
+import morof6 from '../assets/Serien_Karl/Morof/6.jpg';
+import morof7 from '../assets/Serien_Karl/Morof/7.jpg';
+import morof8 from '../assets/Serien_Karl/Morof/8.jpg';
+import morof9 from '../assets/Serien_Karl/Morof/9.jpg';
 
 export const Morof = [
     {
         images:
-            [morof1, morof2, morof3, morof4, morof5, ]
+            [morof1, morof2, morof3, morof4, morof5, morof6, morof7, morof8, morof9, ]
     },
     {
         name:
