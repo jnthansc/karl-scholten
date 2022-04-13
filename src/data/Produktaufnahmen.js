9eb6564ac1773d91cf57ce51617ea239b2c2b3ea
Jonathan <jona.schmidt99@web.de>
2022-04-13 17:11:59 +0200

    adjusted pictures of products

--- src/data/Produktaufnahmen.js
+++ src/data/Produktaufnahmen.js
@@ -1,13 +1,15 @@
 import prod1 from '../assets/Serien_Karl/Produktaufnahmen/1.jpg';
 import prod2 from '../assets/Serien_Karl/Produktaufnahmen/2.jpg';
 import prod3 from '../assets/Serien_Karl/Produktaufnahmen/3.jpg';
+import prod4 from '../assets/Serien_Karl/Produktaufnahmen/4.jpg';
 import prod5 from '../assets/Serien_Karl/Produktaufnahmen/5.jpg';
 import prod6 from '../assets/Serien_Karl/Produktaufnahmen/6.jpg';
+import prod7 from '../assets/Serien_Karl/Produktaufnahmen/7.jpg';
 
 export const Produktaufnahmen = [
     {
         images:
-            [prod1, prod2, prod3, prod5, prod6,]
+            [prod1, prod2, prod3, prod4, prod5, prod6, prod7, ]
     },
     {
         name:
